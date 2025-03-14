d92a2157830bc0810ccbb2a673a37b1c30e1e566
replicantStarr <nicholass@enets.com.au>
2025-03-15 00:55:58 +1100

    Fixed login and signup page spacing.

--- my-vue-app/src/views/TheLoginPage.vue
+++ my-vue-app/src/views/TheLoginPage.vue
@@ -40,10 +40,13 @@ export default {
 </script>
 
 <template>
-    <v-container>
+    <v-card>
+        <v-card-title>WELCOME TO UNICENTRE</v-card-title>
+    </v-card>
+    <v-container :style="{ width: '600px'}">
         <v-form @submit.prevent="submitLogin">
-            <v-card>
-                <v-card-title>WECLOME</v-card-title>
+            <v-card class="pl-10 pr-10 pb-5">
+                <v-card-title>LOGIN</v-card-title>
                 <v-form>
                     <v-text-field label="Email" type="email" required></v-text-field>
                     <v-text-field label="Password" type="password" required></v-text-field>
@@ -51,7 +54,7 @@ export default {
                     <v-btn color="primary" type="submit" @click="submitLogin" block>Login</v-btn>
                 </v-form>
 
-                <v-card-subtitle>If you don't have an account, you can <v-Btn color="secondary" href="/signup">sign-up</v-Btn> here.</v-card-subtitle>
+                <v-card-subtitle class="mt-5">If you don't have an account, you can <v-Btn color="secondary" href="/signup">sign-up</v-Btn> here.</v-card-subtitle>
             </v-card>
         </v-form>
     </v-container>
--- my-vue-app/src/views/TheSignupPage.vue
+++ my-vue-app/src/views/TheSignupPage.vue
@@ -62,52 +62,54 @@ export default {
 </script>
 
 <template>
-    <v-container>
-        <v-card>
-            <v-card-title>WECLOME</v-card-title>
+    <v-card>
+        <v-card-title>WELCOME TO UNICENTRE</v-card-title>
+    </v-card>
+    <v-container :style="{ width: '600px'}">
+        <v-card class="pl-10 pr-10 pb-5">
+            <v-card-title>SIGN-UP</v-card-title>
             <v-form @submit.prevent="submitSignUp">
-                <v-card>
-                    <v-row>
-                        <v-col cols="12" sm="6">
+                <v-carousel
+                    :continuous="false"
+                    :show-arrows="false"
+                >
+                    <v-carousel-item>
                             <v-text-field label="Forename" v-model="forename" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6">
                             <v-text-field label="Surname" v-model="surname" required></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-text-field label="Username" v-model="username" required></v-text-field>
-                    <v-text>Date Of Birth</v-text>
-                    <v-row>
-                        <v-col cols="12" sm="2">
-                            <v-select 
-                                label="Year"
-                                v-model="birthYear"
-                                :items="years">
-                            </v-select>
-                        </v-col>
-                        <v-col cols="12" sm="2">
-                            <v-select 
-                                label="Month"
-                                v-model="birthMonth"
-                                :items="months">
-                            </v-select>
-                        </v-col>
-                        <v-col cols="12" sm="2">
-                            <v-select 
-                                label="Day"
-                                v-model="birthDay"
-                                :items="days">
-                            </v-select>
-                        </v-col>
-                    </v-row>
-                    <v-text-field label="Major" v-model="major" required></v-text-field>
-                </v-card>
-                <v-card>
-                    <v-text-field label="Email" v-model="email" type="email" required></v-text-field>
-                    <v-text-field label="Password" v-model="password" type="password" required></v-text-field>
-                    <v-text-field label="Confirm Password" v-model="confirmPassword" type="password" required></v-text-field>     
-                    <v-btn color="primary" type="submit" @click="submitSignUp">Sign-Up</v-btn>    
-                </v-card>
+                            <v-text-field label="Username" v-model="username" required></v-text-field>
+                            <v-text>Date Of Birth</v-text>
+                            <v-row>
+                                <v-col cols="12" sm="4">
+                                    <v-select 
+                                        label="Year"
+                                        v-model="birthYear"
+                                        :items="years">
+                                    </v-select>
+                                </v-col>
+                                <v-col cols="12" sm="4">
+                                    <v-select 
+                                        label="Month"
+                                        v-model="birthMonth"
+                                        :items="months">
+                                    </v-select>
+                                </v-col>
+                                <v-col cols="12" sm="4">
+                                    <v-select 
+                                        label="Day"
+                                        v-model="birthDay"
+                                        :items="days">
+                                    </v-select>
+                                </v-col>
+                            </v-row>
+                            <v-text-field label="Major" v-model="major" required></v-text-field>
+                    </v-carousel-item>
+                    <v-carousel-item>
+                        <v-text-field label="Email" v-model="email" type="email" required></v-text-field>
+                        <v-text-field label="Password" v-model="password" type="password" required></v-text-field>
+                        <v-text-field label="Confirm Password" v-model="confirmPassword" type="password" required></v-text-field>     
+                        <v-btn color="primary" type="submit" @click="submitSignUp" block>Sign-Up</v-btn>
+                    </v-carousel-item>
+                </v-carousel>
             </v-form>
         </v-card>
     </v-container>
