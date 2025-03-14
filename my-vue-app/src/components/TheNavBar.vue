5551d405f0e7b83d7061c7de5225d99d53b7a671
dqclqn <dqclqn@gmail.com>
2025-03-15 00:01:29 +1100

    better bottom bar

---
+++ my-vue-app/src/components/TheNavBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+var menuItems = ref([
+  { title: "Home", path: "/", icon: "mdi-home" },
+  { title: "Societies", path: "/societies", icon: "mdi-basketball" },
+  { title: "Communities", path: "/communities", icon: "mdi-account-multiple" },
+]);
+</script>
+
+<template>
+  <v-bottom-navigation elevation="12">
+    <v-spacer></v-spacer>
+      <v-btn v-for="item in menuItems" :key="item.title" :to="item.path">
+        <v-icon>{{ item.icon }}</v-icon>
+        <p>{{ item.title }}</p>
+      </v-btn>
+    <v-spacer></v-spacer>
+  </v-bottom-navigation>
+</template>
--- my-vue-app/src/main.ts
+++ my-vue-app/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css';
 
 import { createApp } from 'vue';
+import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
