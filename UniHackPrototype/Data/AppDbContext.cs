428ebaedb7d69b7343f51b559c807d7dbf52dbca
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-14 20:09:37 +1100

    Added dbsets

--- UniHackPrototype/Data/AppDbContext.cs
+++ UniHackPrototype/Data/AppDbContext.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyAspNetVueApp.Models;
+using UniHackPrototype.Models;
 
 namespace MyAspNetVueApp.Data
 {
     public class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Society> Societies { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
