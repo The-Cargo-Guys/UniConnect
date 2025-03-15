384d54f7e3f71edff4cb18aa1fe721521a081759
Daniel Ajami <daniel.ajami025@gmail.com>
2025-03-15 15:18:28 +1100

    Revert "societies details"

--- my-vue-app/src/views/index.ts
+++ my-vue-app/src/views/index.ts
@@ -4,22 +4,5 @@ import TheSocietiesPage from './TheSocietiesPage.vue';
 import TheAddSocietyPage from './TheAddSocietyPage.vue';
 import TheRegistryPage from './TheRegistryPage.vue';
 import TheCallbackPage from "./TheCallbackPage.vue";
-import SocietyDetailsPage from './SocietyDetailsPage.vue';
-import { createRouter as createVueRouter, createWebHistory } from 'vue-router';
 
-const routes = [
-    { path: "/", redirect: "/home" },
-    { path: "/registry", component: TheRegistryPage },
-    { path: "/home", component: TheHomePage },
-    { path: "/societies", component: TheSocietiesPage },
-    { path: "/add-society", component: AddSocietyPage },
-    { path: "/societies/:id", name: "SocietyDetails", component: SocietyDetailsPage },
-];
-
-const router = createVueRouter({
-    history: createWebHistory(),
-    routes,
-});
-
-export { TheCommunitiesPage, TheHomePage, TheSocietiesPage, TheRegistryPage, AddSocietyPage, TheCallbackPage, SocietyDetailsPage };
-export default router;
+export { TheCommunitiesPage, TheHomePage, TheSocietiesPage, TheRegistryPage, TheAddSocietyPage, TheCallbackPage };
