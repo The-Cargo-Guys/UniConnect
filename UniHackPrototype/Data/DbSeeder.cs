358afe0254fb427029e28355b269c7a05509a155
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-15 16:17:20 +1100

    added heaps of seed data

--- UniHackPrototype/Data/DbSeeder.cs
+++ UniHackPrototype/Data/DbSeeder.cs
@@ -21,87 +21,98 @@ namespace UniHack.Data
 
             if (!_context.Users.Any())
             {
-                var user = new User
+                var tags = new List<Tag>();
+                for (int i = 1; i <= 50; i++)
                 {
-                    Name = "Admin",
-                    Email = "admin@example.com",
-                    Password = "password123",
-                    IsAdmin = true,
-                    PhoneNumber = "0457174936",
-                    University = "University of Melbourne",
-                    Degree = "Bachelor of Science",
-                    Bio = "I am the admin",
-                    ImagePath = "https://example.com/admin.jpg",
-                    Tags = new List<Tag> { new Tag { Value = "Rugby" }, new Tag { Value = "Sports" } }
-                };
-                _context.Users.Add(user);
+                    tags.Add(new Tag { Value = $"Tag-{i}" });
+                }
+                _context.Tags.AddRange(tags);
                 _context.SaveChanges();
 
-                var user2 = new User
+                var users = new List<User>();
+                for (int i = 1; i <= 10; i++)
                 {
-                    Name = "NON_Admin",
-                    Email = "admin@example.com",
-                    Password = "password123",
-                    IsAdmin = false,
-                    PhoneNumber = "04563894936",
-                    University = "University of sydney",
-                    Degree = "Bachelor of arts",
-                    Bio = "I am not the admin",
-                    ImagePath = "https://example.com/admin.jpg",
-                    Tags = new List<Tag> { new Tag { Value = "phones" }, new Tag { Value = "swimming" } }
-                };
-                _context.Users.Add(user2);
+                    users.Add(new User
+                    {
+                        Name = $"User-{i}",
+                        Email = $"user{i}@example.com",
+                        Password = "password123",
+                        IsAdmin = i == 1, // First user is admin
+                        PhoneNumber = $"04571749{i:D2}",
+                        University = i % 2 == 0 ? "University of Melbourne" : "University of Sydney",
+                        Degree = i % 2 == 0 ? "Bachelor of Science" : "Bachelor of Arts",
+                        Bio = $"This is user-{i}",
+                        ImagePath = $"https://example.com/user{i}.jpg",
+                        Tags = tags.OrderBy(x => Guid.NewGuid()).Take(5).ToList()
+                    });
+                }
+                _context.Users.AddRange(users);
                 _context.SaveChanges();
 
-                var tag = new Tag
+                var courses = new List<Course>();
+                for (int i = 1; i <= 10; i++)
                 {
-                    Value = "Technology"
-                };
-                _context.Tags.Add(tag);
+                    courses.Add(new Course
+                    {
+                        Name = $"Course-{i}",
+                        Description = $"Description for course-{i}",
+                        ImagePathBanner = $"https://example.com/course-banner{i}.jpg",
+                        Tags = tags.OrderBy(x => Guid.NewGuid()).Take(3).ToList(),
+                        Members = users.OrderBy(x => Guid.NewGuid()).Take(5).ToList()
+                    });
+                }
+                _context.Courses.AddRange(courses);
                 _context.SaveChanges();
 
-                var comment = new Comment
+                var societies = new List<Society>();
+                for (int i = 1; i <= 10; i++)
                 {
-                    Content = "Great first post!",
-                    UserId = user.Id,
-                    Author = user
-                };
-                _context.Comments.Add(comment);
+                    societies.Add(new Society
+                    {
+                        Name = $"Society-{i}",
+                        Description = $"A society for {i}",
+                        ImagePathBanner = $"https://example.com/society-banner{i}.jpg",
+                        Tags = tags.OrderBy(x => Guid.NewGuid()).Take(3).ToList(),
+                        Members = users.OrderBy(x => Guid.NewGuid()).Take(5).ToList()
+                    });
+                }
+                _context.Societies.AddRange(societies);
                 _context.SaveChanges();
 
-                var course = new Course
+                var comments = new List<Comment>();
+                foreach (var user in users)
                 {
-                    Name = "Computer Science 101",
-                    Description = "An introductory course to computer science.",
-                    ImagePathBanner = "https://example.com/course-banner.jpg",
-                    Tags = new List<Tag> { tag },
-                    Members = new List<User> { user }
-                };
-                _context.Courses.Add(course);
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        comments.Add(new Comment
+                        {
+                            Content = $"Comment {i} by {user.Name}",
+                            UserId = user.Id,
+                            Author = user
+                        });
+                    }
+                }
+                _context.Comments.AddRange(comments);
                 _context.SaveChanges();
 
-                var society = new Society
+                var posts = new List<Post>();
+                foreach (var user in users)
                 {
-                    Name = "Tech Enthusiasts",
-                    Description = "A society for people passionate about technology.",
-                    ImagePathBanner = "https://example.com/society-banner.jpg",
-                    Tags = new List<Tag> { tag },
-                    Members = new List<User> { user }
-                };
-                _context.Societies.Add(society);
-                _context.SaveChanges();
-
-                var post = new Post
-                {
-                    Title = "Welcome Post",
-                    Content = "This is the first post in the system.",
-                    AuthorId = user.Id,
-                    Tags = new List<Tag> { tag },
-                    Comments = new List<Comment> { comment },
-                    Author = user,
-                    Community = course
-                };
-                _context.Posts.Add(post);
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        posts.Add(new Post
+                        {
+                            Title = $"Post {i} by {user.Name}",
+                            Content = $"This is post {i}.",
+                            AuthorId = user.Id,
+                            Tags = tags.OrderBy(x => Guid.NewGuid()).Take(4).ToList(),
+                            Comments = comments.OrderBy(x => Guid.NewGuid()).Take(3).ToList(),
+                            Author = user,
+                            Community = courses.OrderBy(x => Guid.NewGuid()).First()
+                        });
+                    }
+                }
+                _context.Posts.AddRange(posts);
                 _context.SaveChanges();
             }
         }
