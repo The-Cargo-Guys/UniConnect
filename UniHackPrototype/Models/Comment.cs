40add9b5ca7d2ea625068dfd0f57256c379d41f4
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-15 13:49:00 +1100

    fixes

--- UniHackPrototype/Models/Comment.cs
+++ UniHackPrototype/Models/Comment.cs
@@ -1,5 +1,7 @@
 ﻿using MyAspNetVueApp.Models;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniHackPrototype.Models
 {
@@ -7,6 +9,7 @@ namespace UniHackPrototype.Models
     {
         [Key]
         public Guid Id { get; set; }
+
         public string Content { get; set; } = string.Empty;
         public required User Author { get; set; }
     }
