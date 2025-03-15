e8d0cdbd2f7d31ead0a99038165496179be681c5
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-15 23:55:31 +1100

    Added events and comment controller

---
+++ UniHackPrototype/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniHackPrototype.Models
+{
+    public class Event
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImagePathBanner { get; set; } = string.Empty;
+        public DateTime Date { get; set; }
+
+    }
+}
