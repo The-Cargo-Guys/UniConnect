3a2fffe34acc586c96c3ee7d983861a7107b9b35
Daniel Ajami <daniel.ajami@wisetechglobal.com>
2025-03-15 14:01:05 +1100

    fixed nav bar and tools

--- my-vue-app/src/views/index.ts
+++ my-vue-app/src/views/index.ts
@@ -2,16 +2,12 @@ import TheCommunitiesPage from './TheCommunitiesPage.vue';
 import TheHomePage from './TheHomePage.vue';
 import TheSocietiesPage from './TheSocietiesPage.vue';
 import AddSocietyPage from './AddSocietyPage.vue';
-import TheLoginPage from './TheLoginPage.vue';
 import TheRegistryPage from './TheRegistryPage.vue';
-import TheSignupPage from "./TheSignupPage.vue";
 import TheCallbackPage from "./TheCallbackPage.vue";
 import { createRouter as createVueRouter, createWebHistory } from 'vue-router';
 
 const routes = [
     { path: "/", redirect: "/home" },
-    { path: "/login", component: TheLoginPage },
-    { path: "/signup", component: TheSignupPage },
     { path: "/registry", component: TheRegistryPage },
     { path: "/home", component: TheHomePage },
     { path: "/societies", component: TheSocietiesPage },
@@ -23,5 +19,5 @@ const router = createVueRouter({
     routes,
 });
 
-export { TheCommunitiesPage, TheHomePage, TheSocietiesPage, TheLoginPage, TheRegistryPage, TheSignupPage, AddSocietyPage, TheCallbackPage };
+export { TheCommunitiesPage, TheHomePage, TheSocietiesPage, TheRegistryPage, AddSocietyPage, TheCallbackPage };
 export default router;
