5bfb1ded19165bd4e6cd9657c5bfd60295b066ad
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-16 15:28:37 +1100

    Fixes for fyp

--- UniHackPrototype/ForYouPageLogic/ForYouPageLogic.cs
+++ UniHackPrototype/ForYouPageLogic/ForYouPageLogic.cs
@@ -12,6 +12,23 @@ namespace UniHack.ForYouPageNamespace
         {
             _postService = postService;
         }
-        public List<Post> GetForYouPage(User user) => _postService.GetPostsByTags(user.Tags.ToList());
+        public List<Post> GetForYouPage(User user)
+        {
+            List<Post> taggedPosts = _postService.GetPostsByTags(user.Tags.ToList());
+
+            if (taggedPosts.Count >= 50)
+            {
+                return taggedPosts.Take(50).ToList();
+            }
+
+            List<Post> allPosts = _postService.GetAllPosts();
+
+            List<Post> remainingPosts = allPosts.Except(taggedPosts).ToList();
+
+            int remainingCount = 50 - taggedPosts.Count;
+            List<Post> additionalPosts = remainingPosts.Take(remainingCount).ToList();
+
+            return taggedPosts.Concat(additionalPosts).ToList();
+        }
     }
 }
