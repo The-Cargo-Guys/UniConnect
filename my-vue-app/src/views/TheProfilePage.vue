fde136d2604ad98bf4680cf0f84d38b39a1707ac
Jack Wang <Jack.Wang@wisetechglobal.com>
2025-03-16 12:46:49 +1100

    created profile page for testing

--- my-vue-app/src/views/TheProfilePage.vue
+++ my-vue-app/src/views/TheProfilePage.vue
@@ -1,5 +1,39 @@
 <template>
     <div>
+      <div class="LoginContainer">
+        <div class="LoginBox">
+          <h1>My Details</h1>
+          <div class="ItemGroups">
+            <label>Profile Picture</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Username: </label> {{ user.name }}
+          </div>
+          <div class="ItemGroups">
+            <label>First Name</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Last Name</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Email</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Phone Number</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Uni</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Degree</label>
+          </div>
+          <div class="ItemGroups">
+            <label>Bio</label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
       <button class="edit-user" type="button" @click="goToEditUserPage" aria-label="Edit User Info">
         Edit Info
       </button>
@@ -8,12 +42,37 @@
   
   <script setup lang="ts">
   import { useRouter } from 'vue-router';
+  import { User } from "../apiClient";
+  import { defineComponent, ref, onMounted } from 'vue';
+  import axios from 'axios';
   
   const router = useRouter();
   
   const goToEditUserPage = () => {
     router.push('/edit-user');
   };
+
+    const user = ref(User);
+    const loading = ref(false);
+    const error = ref('');
+
+    const fetchUserInfo = async () => {
+      loading.value = true;
+      try {
+        const response = await axios.get('/api/GetCurrentUser/');
+        user.value = response.data;
+      } catch (err) {
+        console.error('API Error:', err);
+        error.value = 'Failed to load user.';
+      } finally {
+        loading.value = false;
+      }
+    };
+    
+    onMounted(() => {
+      fetchUserInfo();
+    });
+  
   </script>
   
   <style scoped>
