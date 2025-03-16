7855d3d5314df2dcac3d1a0b215f2e5c9bcc17b1
dqclqn <dqclqn@gmail.com>
2025-03-16 14:17:42 +1100

    Add usernavbar

--- my-vue-app/src/components/TheNavBar.vue
+++ my-vue-app/src/components/TheNavBar.vue
@@ -9,36 +9,36 @@ var menuItems = ref([
 ]);
 </script>
 <template>
-  <div class="wrapper">
-    <v-bottom-navigation
-      grow
-      class="bottom-nav"
-      :elevation="24"
-      mode="shift"
-      rounded="xl"
-    >
-      <v-spacer></v-spacer>
-      <v-btn v-for="item in menuItems" :key="item.title" :to="item.path">
-        <v-icon>{{ item.icon }}</v-icon>
-        <span>{{ item.title }}</span>
-      </v-btn>
-      <v-spacer></v-spacer>
-    </v-bottom-navigation>
-  </div>
+	<div class="wrapper">
+		<v-bottom-navigation
+			grow
+			class="bottom-nav"
+			:elevation="24"
+			mode="shift"
+			rounded="xl"
+		>
+			<v-spacer></v-spacer>
+			<v-btn v-for="item in menuItems" :key="item.title" :to="item.path">
+				<v-icon>{{ item.icon }}</v-icon>
+				<span>{{ item.title }}</span>
+			</v-btn>
+			<v-spacer></v-spacer>
+		</v-bottom-navigation>
+	</div>
 </template>
 
 <style scoped>
 .wrapper {
-  position: relative;
-  display: flex;
-  justify-content: center;
+	position: relative;
+	display: flex;
+	justify-content: center;
 }
 
 .bottom-nav {
-  margin-bottom: 10px;
-  width: 800px !important;
-  left: auto !important;
-  right: auto !important;
-  z-index: 1000;
+	margin-bottom: 10px;
+	width: 800px !important;
+	left: auto !important;
+	right: auto !important;
+	z-index: 1000;
 }
 </style>
---
+++ my-vue-app/src/components/TheUserNavBar.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { defineEmits } from "vue";
+
+const emit = defineEmits(["logout"]);
+</script>
+<template>
+	<div class="wrapper">
+		<v-toolbar floating class="nav" :elevation="24" mode="shift" rounded="xl">
+			<v-spacer></v-spacer>
+			<v-btn to="/profile" flat>
+				<v-icon>mdi-account</v-icon>
+				<span>Profile</span>
+			</v-btn>
+			<v-btn @click="$emit('logout')" flat>Log Out</v-btn>
+			<v-spacer></v-spacer>
+		</v-toolbar>
+	</div>
+</template>
+
+<style scoped>
+.wrapper {
+	position: relative;
+	display: flex;
+	justify-content: center;
+}
+
+.nav {
+	position: fixed;
+	margin-bottom: 10px;
+	max-width: 300px;
+	right: 0.5% !important;
+	z-index: 1000;
+	top: 10px !important;
+}
+</style>
