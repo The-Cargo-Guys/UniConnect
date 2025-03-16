282ab30ddf764c3d1af22410b6f5e0aa8f4ea9dc
Valin-Kennedy <valin.kennedy14@gmail.com>
2025-03-16 11:36:27 +1100

    Added user controller

--- UniHackPrototype/Controllers/UserController.cs
+++ UniHackPrototype/Controllers/UserController.cs
@@ -1,32 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
 using MyAspNetVueApp.Data;
 using MyAspNetVueApp.Models;
+using System;
+using System.Linq;
+using UniHack.Services.Interfaces;
 
 namespace UniHack.Controllers
 {
     [ApiController]
-    [Route("api/users")]  
+    [Route("api/users")]
     public class UserController : ControllerBase
     {
-        private readonly AppDbContext _context;
+        private readonly IUserService _userService;
 
-        public UserController(AppDbContext context)
+        public UserController(IUserService userService)
         {
-            _context = context;
+            _userService = userService;
         }
 
         [HttpGet]
         public IActionResult GetUsers()
         {
-            return Ok(_context.Users.ToList());
+            return Ok(_userService.GetAllUsers());
         }
 
         [HttpPost]
         public IActionResult AddUser([FromBody] User user)
         {
-            _context.Users.Add(user);
-            _context.SaveChanges();
+            _userService.CreateUser(user);
             return CreatedAtAction(nameof(GetUsers), new { id = user.Id }, user);
+        }
+
+        [HttpGet("current")]
+        [Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            if (string.IsNullOrEmpty(userId) || !Guid.TryParse(userId, out var userGuid))
+            {
+                return Unauthorized();
+            }
+
+            var user = _userService.GetUserById(userGuid);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            return Ok(user);
         }
     }
 }
